71c5429e67f5ea8fa2740699f0c7dde7e174ea76
Tri S <mas.trisutrisno@gmail.com>
2019-09-16 08:49:21 +0700

    Update router.js

--- src/router.js
+++ src/router.js
@@ -2,6 +2,7 @@ import Vue from 'vue'
 import Router from 'vue-router'
 // import Home from './views/Home.vue'
 
+import PersonnelLogin from "@/views/LoginPersonnel"
 import Personnel from "@/views/Personnel"
 import PersonnelDashboard from "@/components/personnel/PersonnelDashboard"
 
@@ -12,6 +13,14 @@ import MentorPhaseScore from "@/components/personnel/mentor/phase/score/Score"
 import MentorPhaseScoring from "@/components/personnel/mentor/phase/scoring/Scoring"
 import MentorPhaseScoringParticipant from "@/components/personnel/mentor/phase/scoring/ScoringParticipant"
 
+import Coordinatorship from "@/components/personnel/coordinator/Coordinatorship"
+import CoordinatorProgramPartisipant from "@/components/personnel/coordinator/participant/Participant"
+import CoordinatorProgramRegistration from "@/components/personnel/coordinator/registration/Registration"
+import CoordinatorProgramPhase from "@/components/personnel/coordinator/phase/Phase"
+import CoordinatorProgramPhaseMentoring from "@/components/personnel/coordinator/phase/mentoring/Mentoring"
+import CoordinatorProgramPhaseScoring from "@/components/personnel/coordinator/phase/scoring/Scoring"
+import CoordinatorProgramPhaseScoringRecap from "@/components/personnel/coordinator/phase/recap/Recap"
+
 Vue.use(Router)
 
 export default new Router({
@@ -20,6 +29,11 @@ export default new Router({
       path: '/',
       name: 'home',
       component: Personnel
+    },
+    {
+      path: '/personnel/login',
+      name: 'Login Personnel',
+      component: PersonnelLogin
     },
     {
       path: '/personnel',
@@ -60,6 +74,41 @@ export default new Router({
           path: "/personnel/mentor/:mentorId/program/:programId/phase/:phaseId/scoring/:scoringId/template/:templateId",
           name: "Mentor Phase Scoring Participant",
           component: MentorPhaseScoringParticipant
+        },
+        {
+          path: "/personnel/coordinator",
+          name: "Coordinator Dashboard",
+          component: Coordinatorship
+        },
+        {
+          path: "/personnel/coordinator/:coordinatorId/program/:programId/participant",
+          name: "Program Participant",
+          component: CoordinatorProgramPartisipant
+        },
+        {
+          path: "/personnel/coordinator/:coordinatorId/program/:programId/registration",
+          name: "Program Registration",
+          component: CoordinatorProgramRegistration
+        },
+        {
+          path: "/personnel/coordinator/:coordinatorId/program/:programId/phase",
+          name: "Program Phase",
+          component: CoordinatorProgramPhase
+        },
+        {
+          path: "/personnel/coordinator/:coordinatorId/program/:programId/phase/:phaseId/mentoring",
+          name: "Program Phase Mentoring",
+          component: CoordinatorProgramPhaseMentoring
+        },
+        {
+          path: "/personnel/coordinator/:coordinatorId/program/:programId/phase/:phaseId/scoring",
+          name: "Program Phase Scoring",
+          component: CoordinatorProgramPhaseScoring
+        },
+        {
+          path: "/personnel/coordinator/:coordinatorId/program/:programId/phase/:phaseId/scoring_recap",
+          name: "Program Phase Scoring Recap",
+          component: CoordinatorProgramPhaseScoringRecap
         },
       ]
     },
