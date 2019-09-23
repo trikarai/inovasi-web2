57bacf1c4b2d7268f3d1a001bb928ff6533663eb
Tri S <mas.trisutrisno@gmail.com>
2019-09-23 11:39:19 +0700

    Update router.js

--- src/router.js
+++ src/router.js
@@ -43,6 +43,8 @@ import CoordinatorProgramPhaseScoringRecap from "@/components/personnel/coordina
 Vue.use(Router)
 
 export default new Router({
+  // mode: "history",
+  // base: process.env.BASE_URL,
   routes: [
     {
       path: '/',
