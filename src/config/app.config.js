7bdd25392204ecbde8d5f70662e011271e9e0caa
Tri S <mas.trisutrisno@gmail.com>
2019-09-23 19:57:52 +0700

    Update app.config.js

--- src/config/app.config.js
+++ src/config/app.config.js
@@ -3,5 +3,6 @@ if (process.env.NODE_ENV === 'production') {
   baseUrl = 'https://start.mikti.id/inovasi'
 } else {
   baseUrl = 'https://cors-anywhere.herokuapp.com/https://start.mikti.id/inovasi'
+  // baseUrl = 'https://start.mikti.id/inovasi'
 }
 export const baseUri = baseUrl
