cf355907176a99ed0e422f00b078fc29d29de0de
Tri S <mas.trisutrisno@gmail.com>
2019-09-25 18:10:46 +0700

    add dependeny
    uuid
    moment
    lodash

--- src/main.js
+++ src/main.js
@@ -5,6 +5,9 @@ import store from './store'
 import vuetify from './plugins/vuetify';
 import axios from 'axios';
 import VueAxios from 'vue-axios';
+import UUID from 'vue-uuid';
+import Moment from 'vue-moment'
+import VueLodash from 'vue-lodash'
 
 //global component
 import Notification from "@/components/Notification";
@@ -24,6 +27,11 @@ Vue.mixin({
   }
 })
 
+const options = { name: 'lodash' } // customize the way you want to call it
+Vue.use(VueLodash, options) // options is optional
+
+Vue.use(UUID);
+Vue.use(Moment);
 Vue.use(VueAxios, axios);
 Vue.config.productionTip = false
 
