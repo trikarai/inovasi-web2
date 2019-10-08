64360d1106fce5053df9601f7f77904ab13c2377
Tri S <mas.trisutrisno@gmail.com>
2019-10-08 17:22:14 +0700

    Update validationMixins.js

--- src/mixins/validationMixins.js
+++ src/mixins/validationMixins.js
@@ -8,6 +8,14 @@ export const validationMixins = {
                 v => !!v || "Name is required",
                 v => v.length >= 3 || "Name must be more than 3 characters"
             ],
+            rulesPhone: [
+                v => !!v || "Phone Number is required",
+                v => {
+                    const pattern = /^[0-9]*$/;
+                    return pattern.test(v) || "Number Only";
+                }
+            ],
+            phoneMask: "(###) ##########",
             rulesEmail: [v => !!v || "E-mail is required",
             value => {
                 const pattern = /^(([^<>()[\]\\.,;:\s@"]+(\.[^<>()[\]\\.,;:\s@"]+)*)|(".+"))@((\[[0-9]{1,3}\.[0-9]{1,3}\.[0-9]{1,3}\.[0-9]{1,3}])|(([a-zA-Z\-0-9]+\.)+[a-zA-Z]{2,}))$/;
@@ -17,7 +25,12 @@ export const validationMixins = {
             rulesPassword: [
                 value => !!value || "Password Required.",
                 v => v.length >= 8 || "Min 8 characters"
-            ]
+            ],
+            rulesPasswordConfirmation: [
+                v => !!v || "Confirmation Password is required",
+                () =>
+                    this.cpassword === this.signup.password || "Password does not match"
+            ],
         };
     },
 };
